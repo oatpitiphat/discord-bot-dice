e1fb5d4423cad93b491c84c8729acd5cf3f7d417
oat.pitihat <freedom.oatz@hotmail.com>
2020-07-29 22:39:11 +0700

    OAT | Use .env to config bot token

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+const dotenv = require('dotenv');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+//init environment config
+dotenv.config(); 
 client.on('ready', () => {
   console.log('Please type /dice to roll a dice!');
 });
 
 client.on('message', message => {
-
   if (message.content === '/dice') {
     const dice = random(1, 6);
-    message.channel.send(`> ${dice}`);
+    message.reply(dice);
   }
 });
-
-client.login('NzM4MDM4NjA2NDM4MDcyMzgy.XyGGAw.zMnevysikHGEl7uv6serTDYx-uQ');
+client.login(process.env.BOT_TOKEN);
 
 const random = (min, max) => (Math.floor(
   Math.random() * (max - min) + min
