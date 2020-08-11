e4e6e5b2e5dafbc89bfe49989603102bebd5e1e5
oat.pitihat <freedom.oatz@hotmail.com>
2020-08-12 02:00:51 +0700

    OAT | Add dockerfile #2

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const dotenv = require('dotenv');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const http = require('http');
+
+if (process.env.NODE_ENV !== 'production') {
+  //init environment config
+  dotenv.config();
+}
+
+http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-type': 'text/plain'
+  });
+  res.write(`DICE RANDOM BOT FOR DISCORD`);
+  res.write(`JUST TYPE /dice IN YOUR TEXT CHANNEL`);
+  res.end();
+}).listen(process.env.PORT || 3000)
+
+
+client.on('ready', () => {
+  console.log('Please type /dice to roll a dice!');
+});
+
+client.on('message', message => {
+  if (message.content === '/dice') {
+    const dice = random(1, 6);
+    message.reply(dice);
+  }
+});
+
+
+const random = (min, max) => (Math.floor(
+  Math.random() * (max - min) + min
+));
+
+
+
+
+client.login(process.env.BOT_TOKEN);
